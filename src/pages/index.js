23f4c86066d3144077c75065d9620198b911ea83
saracsilva <saracarolinasilva@gmail.com>
2022-12-12 17:13:50 +0100

    start

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+function index() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default index;
