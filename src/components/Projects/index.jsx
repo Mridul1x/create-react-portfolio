83b1d0ecf0e816be40e2fe8cc0d6d3cec07bfee8
Mridul1x <mridulhasannn@gmail.com>
2023-07-31 01:21:01 +0600

    Text changed

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -101,7 +101,7 @@ function Projects() {
             className="flex uppercase justify-center text-center items-center p-10 bg-light-pink
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold text-purple"
           >
-            Geometric abstract
+            Quiling Art
           </div>
           <Project
             title="Rain Abstract"
