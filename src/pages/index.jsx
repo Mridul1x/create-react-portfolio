9c2de21d44ce5e05d120b9a8609cd6550c95b53e
Mridul1x <mridulhasannn@gmail.com>
2023-07-31 01:27:05 +0600

    Text changed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,14 +4,12 @@ import React from "react";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Gallery from "../components/Gallery/Gallery";
-import Video from "../components/Video/Video";
 function Home() {
   return (
     <div className="flex  flex-col items-center">
       <Hero />
       <Projects />
       <Gallery />
-      <Video />
     </div>
   );
 }
